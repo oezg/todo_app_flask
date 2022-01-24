ecb3ae649d15698123108aeaa1932c2a2aad1fec
Özgür Yildirim <67658548+oz-y@users.noreply.github.com>
2022-01-24 13:10:43 +0100

    delete function debugged

---
+++ app.py
@@ -0,0 +1,74 @@
+import sys
+from black import traceback
+from flask import Flask, jsonify, redirect, render_template, request, url_for, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ozg:ö@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Todo {self.id} {self.description}>"
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description, completed=False)
+        db.session.add(todo)
+        db.session.commit()
+        body['id'] = todo.id
+        body['completed'] = todo.completed
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except Exception as e:
+        print('Error', e)
+        traceback.print_exc()
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({'success': True})
+        
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.order_by('id').all())
